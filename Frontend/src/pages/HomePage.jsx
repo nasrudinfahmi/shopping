8e97858e0ec0af78890dee3ee273a36d7ac4f2ee
nasrudinfahmi <nasrudin.fhm@gmail.com>
2024-01-29 10:26:10 +0700

    feat: update HomePage layout with NavbarBottom integration
    - Integrated NavbarBottom into PageLayout on HomePage.

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,11 +1,12 @@
-import HeroBanner from "../components/fragments/HeroBanner/HeroBanner"
+import HeroBanner from "../components/fragments/heroSect/HeroBanner/HeroBanner"
 import CategoriesSect from "../components/fragments/Categories/CategoriesSect"
 import RecomSect from "../components/fragments/recommendation/RecomSect"
 import PageLayout from "../components/layouts/PageLayout"
+import NavbarBottom from '../components/fragments/navbar/NavbarBottom/NavbarBottom'
 
 function HomePage() {
   return (
-    <PageLayout>
+    <PageLayout NavBottom={<NavbarBottom />}>
       <HeroBanner />
       <CategoriesSect />
       <RecomSect />
